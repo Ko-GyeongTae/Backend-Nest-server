6f4953fdd46fd1659d74d51aad23555c826e2e8b
KoGyeongtae <kokt0203@naver.com>
2021-10-21 15:46:50 +0900

    EDIT - auth api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { SignupDto } from './dto/signup.dto';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('/api/v1/auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('signup')
+    signup(@Body() body: SignupDto) {
+        return this.authService.signup(body);
+    }
+
+    @Post('login')
+    login(@Body() body: LoginDto) {
+        return this.authService.login(body);
+    }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entity/user.entity';
+import { JwtStrategy } from 'src/middleware/jwt.strategy';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: process.env.JWT_KEY,
+      signOptions: { expiresIn: '1h' },
+    })
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy]
 })
 export class AuthModule {}
